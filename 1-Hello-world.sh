749a774f8599fa02eb1521cb336ec85b6caef5be
shalini-9393 <shalinithatikonda73@gmail.com>
2026-04-21 12:08:14 +0530

    Change greeting to 'Hello World!' and add comment
    Updated the greeting message and added a comment.

--- 1-Hello-world.sh
+++ 1-Hello-world.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-echo "hello-world"
+# This is the comment, this will not be executed by bash. This is only for information purpose
+echo "Hello World!"
 
 echo "Hi, I am learning Git"
